042084ecbb1e2b213b5566f4a595531fa77e5ec1
Trevor Allen <trevor2492@gmail.com>
2021-07-14 18:39:10 -0500

    added info icon with hover popover containing useful information

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import CharacterGrid from "./components/CharacterGrid";
 import token from "./token";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import Popover from "@material-ui/core/Popover";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 
 function App() {
   const [characters, setCharacters] = useState("");
@@ -21,6 +25,28 @@ function App() {
     fetchData();
   }, [query]);
 
+  const useStyles = makeStyles((theme) => ({
+    popover: {
+      pointerEvents: "none",
+    },
+    paper: {
+      padding: theme.spacing(1),
+    },
+  }));
+
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handlePopoverOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+
   return (
     <div className="App">
       <input
@@ -29,6 +55,42 @@ function App() {
         onChange={(e) => setQuery(e.target.value)}
         style={{ marginTop: 10 }}
       />
+
+      <div style={{ position: "absolute", right: 20, top: 20 }}>
+        <InfoOutlinedIcon />
+      </div>
+
+      <div>
+        <Typography
+          aria-owns={open ? "mouse-over-popover" : undefined}
+          aria-haspopup="true"
+          onMouseEnter={handlePopoverOpen}
+          onMouseLeave={handlePopoverClose}
+        >
+          Hover with a Popover.
+        </Typography>
+        <Popover
+          id="mouse-over-popover"
+          className={classes.popover}
+          classes={{
+            paper: classes.paper,
+          }}
+          open={open}
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          onClose={handlePopoverClose}
+          disableRestoreFocus
+        >
+          <Typography>I use Popover.</Typography>
+        </Popover>
+      </div>
 
       {characters ? (
         <CharacterGrid characters={characters} />
